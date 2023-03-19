30fbf78526c40cdee2fa8410e3e2d9d34bc680d8
Gabrielle Toutin <gctoutin@gmail.com>
2023-03-19 13:20:14 -0500

    Pushes data to webhook upon applicant create
    But stuff is commented out and the actual webhook isn't working

--- src/api/applicant/content-types/applicant/lifecycles.js
+++ src/api/applicant/content-types/applicant/lifecycles.js
@@ -1,13 +1,12 @@
 const _ = require('lodash');
 const { createModuleResolutionCache } = require("typescript");
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
-// TODO: install discord-webhook-node in npm
 // create new Webhook
 const hook = new Webhook(
   process.env.OPEN_POSITIONS_DISCORD_WEBHOOK
 );
 
-hook.setAuthor("New Applicant Notifier", 'https://avatars.githubusercontent.com/u/53379976?s=200&v=4');
+// hook.setAuthor("New Applicant Notifier", 'https://avatars.githubusercontent.com/u/53379976?s=200&v=4');
 
 const formatJSON = (jsonData) => {
   return `\`\`\`JSON\n${JSON.stringify(jsonData, null, 1)}\n\`\`\``;
@@ -22,19 +21,25 @@ module.exports = {
     const applicantProject = await strapi.db.query('api::project.project').findOne({
       where: { slug: project }
     });
-    const projectID = applicantProject.id;
-    event.params.data.project = projectID
+    console.log(applicantProject);
+    try {
+      const projectID = applicantProject.id;
+      event.params.data.project = projectID
+    } 
+    catch (e) {
+      console.log("couldn't find projectID")
+    }
   },
   async afterCreate(event) {
     // use event object https://docs.strapi.io/dev-docs/backend-customization/models#hook-event-object
     // FIXME: remove debug code
     console.log(event);
 
-    const relevantInfo = _.omit(event.data.attributes, ['email', 'age', 'zip']);
+    const relevantInfo = _.omit(event.model.attributes, ['email', 'age', 'zip']);
 
     const newApplicantMsg = new MessageBuilder()
       .setTitle("New Applicant!")
-      .setURL(`${process.env.FRONTEND_URL}/projects/${event.data.project}`)
+      .setURL(`${process.env.FRONTEND_URL}/projects/${event.model.project}`)
       .setDescription(
         formatJSON(relevantInfo)
       );

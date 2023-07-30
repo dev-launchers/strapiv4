3281da0412c231212a68605939de58ff34bbbe12
Noel Moon <nmoon778@gmail.com>
2023-07-30 19:55:48 -0400

    Added the email template

--- src/extensions/users-permissions/content-types/user/lifecycles.js
+++ src/extensions/users-permissions/content-types/user/lifecycles.js
@@ -1,11 +1,15 @@
 module.exports = {
   async afterCreate(event) {
+    console.log(event.params.data)
+
     await strapi.plugins['email'].services.email.send({
       to: event.params.data.email,
       from: 'support@devlaunchers.com',
-      subject: 'The Strapi Email plugin worked successfully',
-      text: 'Hello world!',
-      html: 'Hello world!',
+      // subject: 'The Strapi Email plugin worked successfully',
+      template_id: 'd-4671f2012b484ba9890d4bb6a24bc6f3',
+      dynamic_template_data: {
+        username: event.params.data.username,
+      },
     });
   },
 };

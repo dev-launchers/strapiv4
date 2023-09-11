978ee6291e0a3771e17bc460525233d06faae44b
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-09-11 17:55:19 +0100

    controller gets project resource by id

--- src/api/project-resource/controllers/project-resource.js
+++ src/api/project-resource/controllers/project-resource.js
@@ -1,9 +1,49 @@
-'use strict';
+"use strict";
 
 /**
  * project-resource controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::project-resource.project-resource');
+module.exports = createCoreController(
+  "api::project-resource.project-resource",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entries = await strapi.db
+        .query("api::project-resource.project-resource")
+        .findMany({
+          where: {
+            project: {
+              team: {
+                $or: [
+                  {
+                    leaders: {
+                      leader: {
+                        id: id,
+                      },
+                    },
+                  },
+                  {
+                    members: {
+                      member: {
+                        id: id,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+          populate: [
+            "project.team.leaders.leader.profile",
+            "project.team.members.member.profile",
+            "project.heroImage",
+          ],
+        });
+
+      return entries;
+    },
+  })
+);

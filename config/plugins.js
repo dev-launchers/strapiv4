a6e883389c259193b6f95715eac21aa7c0638fe7
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-12-17 23:13:12 +0000

    feat(chungthuang): Produce prometheus metrics for API responses by status code

--- config/plugins.js
+++ config/plugins.js
@@ -32,4 +32,37 @@ module.exports = ({ env }) => ({
       defaultDepth: 3, // Default is 5
     }
   },
+  'strapi-prometheus': {
+    enabled: true,
+    config: {
+      // add prefix to all the prometheus metrics names.
+      prefix: '',
+
+      // use full url instead of matched url
+      // true  => path label: `/api/models/1`
+      // false => path label: `/api/models/:id`
+      fullURL: false,
+
+      // include url query in the url label
+      // true  => path label: `/api/models?limit=1`
+      // false => path label: `/api/models`
+      includeQuery: false,
+
+      // metrics that will be enabled, by default they are all enabled.
+      enabledMetrics: {
+        koa: true, // koa metrics
+        process: false, // metrics regarding the running process
+        http: true, // http metrics like response time and size
+        apollo: false, // metrics regarding graphql
+      },
+
+      // interval at which rate metrics are collected in ms
+      interval: 10_000,
+
+      // set custom/default labels to all the prometheus metrics
+      customLabels: {
+        name: "strapi-prometheus",
+      },
+    }
+  }
 });

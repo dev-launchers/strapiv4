dc3ec3f3e0acb3bc8ff78821652c901e5846b2fd
becevka <becevka@gmail.com>
2024-02-17 23:43:50 -0600

    feat(becevka): Reconfigure Jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  coverageThreshold: {
+    global: {
+      lines: 60,
+    },
+  },
+  collectCoverageFrom: ["src/api/**/*.js"],
+  coverageReporters: ["text"],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ".tmp",
+    "/build/",
+    ".cache",
+  ],
+  testEnvironment: "node",
+};

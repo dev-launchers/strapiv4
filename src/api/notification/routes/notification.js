1e0c25e6a20a5387dfd38e612498cce20a57654b
macbook <macbook@macbooks-MacBook-Pro.local>
2024-05-12 21:25:48 +0100

    Attach policy to notification

--- src/api/notification/routes/notification.js
+++ src/api/notification/routes/notification.js
@@ -6,4 +6,17 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::notification.notification');
+
+module.exports = createCoreRouter('api::notification.notification' , ({ strapi }) => ({
+  routes: [
+    {
+      method: 'GET',
+      path: '/notifications',
+      handler: 'notification.find',
+      config: {
+        // The `isAuthenticated` policy found at `./src/api/notification/policies/isAuthenticated.js` is executed before the `find` action in the `notification.js` controller.
+        policies: ['api::notification.isAuthenticated'] 
+      }
+    }
+  ]
+}));

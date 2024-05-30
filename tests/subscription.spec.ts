a1c053b03565b072e821c9365fd336c496500af6
tiagopazhs <tiagopaz.hs@gmail.com>
2024-05-30 20:45:30 -0300

    feat: Add test to check if the subscription were created after the idea card

---
+++ tests/subscription.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { api } from './utils';
+import { user } from '../init/config';
+
+test.describe('/api/subscriptions', () => {
+    test("Create a subscription when idea is created", async ({ request }) => {
+        const newIdea = await api(request).post("/api/idea-cards", {
+            ideaName: "Testing with subs 1",
+            tagline: "Yay!",
+            description: "I am testing",
+            targetAudience: "Yes",
+            features: "Yes",
+            experience: "Yes",
+            extraInfo: "Yes",
+            involveLevel: "minimum",
+            status: "workshopping",
+        });
+
+        const subscriptions = await api(request).get("/api/subscriptions");
+
+
+        const subscription = subscriptions.data
+            .find(item => (item.attributes.entityId === newIdea.id && item.attributes.entityType === "IdeaCard"));
+        console.log('subscription ->', subscription);
+        // expect(subscription.attributes.user.id).toBe(user.id)
+        expect(subscription.attributes.TimeCreated).toBeDefined();
+        expect(subscription.attributes.active).toBe(true);
+    })
+})

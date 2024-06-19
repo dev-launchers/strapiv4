1b519b9ca7e1db8fcd0bacabdc659cda703276ce
oriyomibadmus <badmus.oriyomi1@gmail.com>
2024-06-19 23:25:57 +0100

    adding policy and test

--- src/api/notification/routes/notification.js
+++ src/api/notification/routes/notification.js
@@ -6,17 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-
-module.exports = createCoreRouter('api::notification.notification' , ({ strapi }) => ({
-  routes: [
-    {
-      method: 'GET',
-      path: '/notifications',
-      handler: 'notification.find',
-      config: {
-        // The `isAuthenticated` policy found at `./src/api/notification/policies/isAuthenticated.js` is executed before the `find` action in the `notification.js` controller.
-        policies: ['api::notification.isAuthenticated'] 
-      }
-    }
-  ]
-}));
+module.exports = createCoreRouter('api::notification.notification');

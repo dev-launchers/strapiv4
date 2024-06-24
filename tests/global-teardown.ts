aba417aeccc9eb8843d31adb47c7cbd0e9881520
becevka <becevka@gmail.com>
2024-06-23 19:25:02 -0500

    fix(test): added comment

--- tests/global-teardown.ts
+++ tests/global-teardown.ts
@@ -2,6 +2,7 @@ import Strapi from "@strapi/strapi";
 import { cleanUpDatabase } from "../init/common";
 
 async function globalTeardown() {
+    // need to keep this line, otherwise github annotations will not work
     console.log('GITHUB_WORKSPACE is set to:', process.env['GITHUB_WORKSPACE']);
     if (process.env.TEST_CONTAINER !== 'true') {
         await Strapi();

ebcb09c2403d83a00caa7ea64d729f33c7726e0d
becevka <becevka@gmail.com>
2024-06-23 19:16:26 -0500

    fix(test): or it is just random

--- tests/global-teardown.ts
+++ tests/global-teardown.ts
@@ -2,6 +2,7 @@ import Strapi from "@strapi/strapi";
 import { cleanUpDatabase } from "../init/common";
 
 async function globalTeardown() {
+    console.log('GITHUB_WORKSPACE is set to:', process.env['GITHUB_WORKSPACE']);
     if (process.env.TEST_CONTAINER !== 'true') {
         await Strapi();
         console.log('Deleting test database...');

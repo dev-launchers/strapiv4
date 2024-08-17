18d65c2d5fe512dfdce3b1e8a181222dced5bc37
chungthuang <7826979+chungthuang@users.noreply.github.com>
2024-08-17 15:46:32 -0500

    feat(notifications): Expose a PUT endpoint to update notification
    This should only be used to update a notification readDataeTime

--- src/api/notification/routes/custom-notification.js
+++ src/api/notification/routes/custom-notification.js
@@ -1,21 +1,29 @@
 module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/notifications',
-        handler: 'notification.find',
-        config: {
-          // Attaching the "is-notification-user" policy found at "./src/api/notification/policies/is-notification-user.js".
-          policies: ['api::notification.notification-owner-filter']
-        }
-      },
-      {
-        method: "GET",
-        path: "/notifications/:id",
-        handler: "notification.findOne",
-        config: {
-          policies: ["api::notification.notification-owner-filter"]
-        }
+  routes: [
+    {
+      method: 'GET',
+      path: '/notifications',
+      handler: 'notification.find',
+      config: {
+        // Attaching the "is-notification-user" policy found at "./src/api/notification/policies/is-notification-user.js".
+        policies: ['api::notification.notification-owner-filter']
       }
-    ]
+    },
+    {
+      method: "GET",
+      path: "/notifications/:id",
+      handler: "notification.findOne",
+      config: {
+        policies: ["api::notification.notification-owner-filter"]
+      }
+    },
+    {
+      method: "PUT",
+      path: "/notifications/:id",
+      handler: "notification.update",
+      config: {
+        policies: ["api::notification.notification-owner-filter"]
+      }
+    },
+  ]
 }

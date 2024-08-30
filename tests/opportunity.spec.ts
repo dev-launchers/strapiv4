70a921bcbd295ee867e29a52e14d4cffe59dbb0a
nikita-pardeshi-github <nikita.pardeshi.it@gmail.com>
2024-08-30 12:34:35 -0700

    update pportunity tests

--- tests/opportunity.spec.ts
+++ tests/opportunity.spec.ts
@@ -7,35 +7,34 @@ test.describe('/api/opportunities', () => {
     test("Get all opportunities", async ({ request }) => {
         const opportunityData = await api(request).getData("/api/opportunities");
 
-        // expect atleast one opportunity exists
-        expect(opportunityData.length).not.toBe(0)
+        // expect two opportunities to exists
+        expect(opportunityData.length).toBe(2)
 
-        const opportunities = opportunityData[0].attributes;
+        const opportunity = opportunityData[0].attributes;
         
         // expect opportunities have different fields filled
-        expect(opportunities.title).toBe(config.opportunityTitles[0]);
-        expect(opportunities.level).toBe(config.opportunity.level);
-        expect(opportunities.commitmentHoursPerWeek).toBe(config.opportunity.commitmentHoursPerWeek);
-        expect(opportunities.description).toBe(config.opportunity.description);
-        expect(opportunities.isHidden).toBe(config.opportunity.isHidden);
-        expect(opportunities.roleCategory).toBe(config.opportunity.roleCategory);
-        expect(opportunities.roleType).toBe(config.opportunity.roleType); 
+        expect(opportunity.title).toBe(config.firstOpportunity.title);
+        expect(opportunity.level).toBe(config.firstOpportunity.level);
+        expect(opportunity.commitmentHoursPerWeek).toBe(config.firstOpportunity.commitmentHoursPerWeek);
+        expect(opportunity.description).toBe(config.firstOpportunity.description);
+        expect(opportunity.isHidden).toBe(config.firstOpportunity.isHidden);
+        expect(opportunity.roleCategory).toBe(config.firstOpportunity.roleCategory);
+        expect(opportunity.roleType).toBe(config.firstOpportunity.roleType); 
     });
     test("Get opportunity by id", async ({ request }) => {
-        const opportunityData = await api(request).getData("/api/opportunities/1");
+        const opportunityData = await api(request).getData("/api/opportunities/2");
         
-        // expect to receive opportunity filtered by id 1
-        expect(opportunityData.id).toBe(1);
-        
-        const opportunities = opportunityData.attributes;
+        // expect to receive opportunity filtered by id 2
+        expect(opportunityData.id).toBe(2);
+        const opportunity = opportunityData.attributes;
 
         // expect opportunities have different fields filled
-        expect(opportunities.title).toBe(config.opportunityTitles[0]);
-        expect(opportunities.level).toBe(config.opportunity.level);
-        expect(opportunities.commitmentHoursPerWeek).toBe(config.opportunity.commitmentHoursPerWeek);
-        expect(opportunities.description).toBe(config.opportunity.description);
-        expect(opportunities.isHidden).toBe(config.opportunity.isHidden);
-        expect(opportunities.roleCategory).toBe(config.opportunity.roleCategory);
-        expect(opportunities.roleType).toBe(config.opportunity.roleType); 
+        expect(opportunity.title).toBe(config.secondOpportunity.title);
+        expect(opportunity.level).toBe(config.secondOpportunity.level);
+        expect(opportunity.commitmentHoursPerWeek).toBe(config.secondOpportunity.commitmentHoursPerWeek);
+        expect(opportunity.description).toBe(config.secondOpportunity.description);
+        expect(opportunity.isHidden).toBe(config.secondOpportunity.isHidden);
+        expect(opportunity.roleCategory).toBe(config.secondOpportunity.roleCategory);
+        expect(opportunity.roleType).toBe(config.secondOpportunity.roleType); 
     }); 
 });

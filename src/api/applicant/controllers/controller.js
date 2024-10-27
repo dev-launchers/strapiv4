9e614b1ae7d5958bbcef84bcc82550ce6c4fe93f
Meenashi <jeyanthi_sm@yahoo.com>
2024-10-27 15:13:46 +0000

    fix:service_account file location change

--- src/api/applicant/controllers/controller.js
+++ src/api/applicant/controllers/controller.js
@@ -1,6 +1,4 @@
 "use strict";
-const path = require("path");
-const process = require("process");
 const myList = require("strapiv4-provider-to-google-drive");
 const { createCoreController } = require("@strapi/strapi").factories;
 const modelUid = "api::applicant.applicant";
@@ -9,54 +7,26 @@ module.exports = createCoreController(
 
   ({ strapi }) => ({
     upload: async (ctx) => {
-      const SERVICE_ACCOUNT_PATH = path.join(
-        process.cwd(),
-        "service_account.json"
-      );
-      console.log(SERVICE_ACCOUNT_PATH);
-
       try {
-        const myall = myList.upload(ctx, SERVICE_ACCOUNT_PATH);
+        const myall = myList.upload(ctx);
         console.table(myall);
         return myall;
-        // ctx.response.data = myall;
-        // ctx.response.status = 200;
-        // ctx.response.message = "Google Upload Response Success";
       } catch (err) {
         ctx.throw(555, err);
       }
     },
     getall: async (ctx) => {
-      const SERVICE_ACCOUNT_PATH = path.join(
-        process.cwd(),
-        "service_account.json"
-      );
-      //console.log(SERVICE_ACCOUNT_PATH);
-
       try {
-        const myall = myList.getAll(SERVICE_ACCOUNT_PATH);
-        //console.table(myall);
+        const myall = myList.getAll();
         return myall;
-        // ctx.response.data = myall;
-        // ctx.response.status = 200;
-        // ctx.response.message = "Google Upload Response Success";
       } catch (err) {
         ctx.throw(555, err);
       }
     },
     delete: async (ctx) => {
-      const SERVICE_ACCOUNT_PATH = path.join(
-        process.cwd(),
-        "service_account.json"
-      );
-      //console.log(SERVICE_ACCOUNT_PATH);
-
       try {
-        const myall = myList.deleteFile(ctx, SERVICE_ACCOUNT_PATH);
+        const myall = myList.deleteFile(ctx);
         return myall;
-        // ctx.response.data = myall;
-        // ctx.response.status = 200;
-        // ctx.response.message = "Google Upload Response Success";
       } catch (err) {
         ctx.throw(555, err);
       }

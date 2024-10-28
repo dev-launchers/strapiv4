bdec61c92c74fc87b4f9002404cfe881b7fedba1
Meenashi <jeyanthi_sm@yahoo.com>
2024-10-28 21:05:10 +0000

    feat(recruit): Create plugin for uploading applicant portfolios to shared Google Drive

---
+++ src/plugins/googledrive/server/services/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const welcomeService = require('./welcome-service');
+const operationService = require('./operation-service');
+
+module.exports = {
+  welcomeService,
+  operationService,
+};

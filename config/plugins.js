9feb341e954fa6514acaacef1a7564663ce3edd8
chungthuang <7826979+chungthuang@users.noreply.github.com>
2026-04-08 19:26:11 -0500

    fix(upload): public access url for upload was using the wrong env var

--- config/plugins.js
+++ config/plugins.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
          * This option is required to upload files larger than 5MB, and is highly recommended to be set.
          * Check the cloudflare docs for the setup: https://developers.cloudflare.com/r2/data-access/public-buckets/#enable-public-access-for-your-bucket
          */
-        cloudflarePublicAccessUrl: env('STORAGE_CF_PUBLIC_ACCESS_URL'),
+        cloudflarePublicAccessUrl: `https://${env('STORAGE_CF_PUBLIC_ACCESS_HOSTNAME')}`,
         /**
          * Sets if all assets should be uploaded in the root dir regardless the strapi folder.
          * It is useful because strapi sets folder names with numbers, not by user's input folder name
